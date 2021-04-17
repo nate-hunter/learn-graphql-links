9582ba0518ebe955d0bede8eb7f788d5449f1b6d
Nate <hunter.j.nate@gmail.com>
2021-04-17 14:44:10 -0400

    basic query feature added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `
 
+const links = [{
+    id: 'link-00',
+    description: 'Where to start to learn GraphQL',
+    url: 'www.howtographql.com'
+}]
+
 const resolvers = {
     Query: {
-        info: () => `This is an app to help manage and track learning GraphQL.`
+        info: () => `This is an app to help manage and track learning GraphQL.`,
+        feed: () => links,
     }
 }
 
@@ -18,7 +32,7 @@ const server = new ApolloServer({
 })
 
 server
-    .listen() 
+    .listen(4005) 
     .then(({ url }) => 
         console.log(`Server is running on ${url}`)
     )
